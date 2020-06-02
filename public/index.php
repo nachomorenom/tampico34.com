a735cd10964c30b4c9237f0335f9c5c4375e96d3
nachomorenom <nachomorenomontesinos@gmail.com>
2020-06-02 13:39:33 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -56,27 +56,35 @@
 
 
 <body>
-	<!-- MENU -->
-	<div class="container-fluid-nacho">
-		<div class="row">
-			<ul>
-				<!--<li class="botones"><a href=”#”>Inicio</a></li>-->
-				<li class="botones"><a href="#caracteristicas">Caracteristicas</a></li>
-				<li class="botones"><a href="#oficinas">Oficinas</a></li>
-				<li class="botones"><a href="#precios">Precios</a></li>
-				<li class="botones"><a href="#contacto">Contacto</a></li>
-			</ul>
-		</div>
 	
-	</div>
-	
-
-
 	<!-- Logo sticky -->
-	<div class="container-fluid sticky-top bg-white">
+	<!--<div class="container-fluid sticky-top bg-white">
 		<div class="row">
 			<div class="col-12 px-5 mt-md-4">
 				<img src="images/Logo Tampico34_ espacio de coworking en Madrid.png" alt="Tampico 34 - Espacio de coworking en Madrid">
+			</div>
+		</div>
+	</div>-->
+	<div class="row">
+		<div class="col-3 px-md-5 mt-md-3">
+			<img src="images/Logo Tampico34_ espacio de coworking en Madrid.png" alt="Tampico 34 - Espacio de coworking en Madrid">
+		</div>
+		<div class="col-9 mt-md-4 mt-2 text-right navbar-expand-md">
+			<nav class="navbar navbar-light" style="display: flex; justify-content: right">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" 
+					data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" 
+						aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</nav>
+			<div class="collapse navbar-collapse float-right" id="navbarToggleExternalContent">
+			<ul>
+				<!--<li class="botones"><a href=”#”>Inicio</a></li>-->
+				<li class="nav-link float-md-left"><a href="#caracteristicas">Caracteristicas</a></li>
+				<li class="nav-link float-md-left"><a href="#oficinas">Oficinas</a></li>
+				<li class="nav-link float-md-left"><a href="#precios">Precios</a></li>
+				<li class="nav-link float-md-left"><a href="#contacto">Contacto</a></li>
+			</ul>
 			</div>
 		</div>
 	</div>
