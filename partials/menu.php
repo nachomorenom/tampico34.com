86a693cb001a3b4305afb4ca9591bba20a5410f3
Fernando Plaza <fernando.plaza@enzo.es>
2020-06-16 10:37:55 +0200

    Cambios en orden carpetas

---
+++ partials/menu.php
@@ -0,0 +1,20 @@
+<div class="col-3 px-md-5 mt-md-3">
+			<img src="/static/img/logo.png" alt="Tampico 34 - Espacio de coworking en Madrid">
+		</div>
+		<div class="col-9 mt-md-4 mt-2 text-right navbar-expand-md">
+			<nav class="navbar navbar-light" style="justify-content: right; position: absolute; right: 0; margin-right: 15px">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" 
+					data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" 
+						aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</nav>
+			<div class="collapse navbar-collapse float-right mt-md-3 mt-5" id="navbarToggleExternalContent">
+				<div>
+					<a data-scroll class="nav-link float-md-left" href="#caracteristicas"><b>Características</b></a>
+					<a class="nav-link float-md-left" href="#oficinas"><b>Oficinas</b></a>
+					<a class="nav-link float-md-left" href="#precios"><b>Precios</b></a>
+					<a class="nav-link float-md-left" href="#contacto"><b>Contacto</b></a>
+	 			</div>
+			</div>
+		</div>
